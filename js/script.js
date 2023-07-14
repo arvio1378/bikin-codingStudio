442b44babbdb1ce3f7c36c138d51835411df307c
arvio1378 <4rv10suhendar@gmail.com>
2023-07-14 07:27:21 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const sidebar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu')
 
 document.querySelector('#bars').onclick = () => {
     sidebar.classList.toggle('active');
@@ -7,3 +8,7 @@ document.querySelector('#bars').onclick = () => {
 document.querySelector('#xmark').onclick = () => {
     sidebar.classList.toggle('active');
 };
+
+document.querySelector('#menu').onclick = () => {
+    menu.classList.toggle('muncul')
+}
